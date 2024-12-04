7c33e8edd75d7433e1551dc49e59a52db654d576
Jonathan <jonathan.bosman@gmail.com>
2024-12-04 15:43:11 +0100

    ajout des packages de sécurité et instructions en commentaires contre les failles générales de sécurité de node

--- back/server.js
+++ back/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 const db = require('./modules/database.js');
-// const usersRoutes = require('./routes/users.js');
+const usersRoutes = require('./routes/users.js');
+const helmet = require('helmet');
 const cors = require('cors');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const path = require('path');
 
 const app = express();
+app.use(helmet());
 app.use(cors({
     origin: 'http://localhost:5173'
 }))
@@ -32,11 +35,20 @@ app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(bodyParser.json());
-// app.use('/api/users', usersRoutes);
+app.use(
+    session({
+    secret: "devSecretKey",
+    name: "SESSID",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { sameSite: true },
+    })
+   );   
+app.use('/api/users', usersRoutes);
 
 db.connect((err) => {
     if (err) {console.log(err);}
-    else {console.log('bravo');}
+    else {console.log('connecté à la base de données');}
 });
 
 const port = process.env.PORT || 3333;
